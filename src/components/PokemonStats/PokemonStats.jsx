36e823f071ff14bdbe8b948e33e66ccbcff0d238
MaximeBjrd <max.beaujard@yahoo.fr>
2025-06-29 14:51:26 +0200

    feat(): Display pokemon stats with a progressbar

---
+++ src/components/PokemonStats/PokemonStats.jsx
@@ -0,0 +1,35 @@
+import style from "./PokemonStats.module.css";
+
+import { MAX_STAT } from "../../constants/app-constants";
+
+const STAT_NAMES = {
+    "HP": "hp",
+    "Attack": "attack",
+    "Defense": "defense",
+    "Special attack": "specialAttack",
+    "Special defense": "specialDefense",
+    "Speed": "speed"
+};
+
+
+export function PokemonStats({ base }) {
+    return <div className={style.statsContainer}>
+        {Object.entries(base).map(([label, value]) => {
+            const key = STAT_NAMES[label];
+            const max = MAX_STAT[key];
+            const progressBarLength = Math.round((value / max) * 100);
+
+            return(
+                <div key={label} className={style.statRow}>
+                    <p className={style.statInfo}> {label} </p>
+                    <p className={style.statInfo}> {value} </p>
+                    
+                    <div className={style.statBar}>
+                        <div className={style.statBarFill} style={{ width: `${progressBarLength}%` }}></div>
+                    </div>
+                </div>
+            )
+        })
+        }
+    </div>
+}
